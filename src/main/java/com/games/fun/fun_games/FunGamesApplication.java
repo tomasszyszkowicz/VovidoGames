144f969b48cd996cc80e064ac2e055e5de600ac4
tomas szyszkowicz <tomasszyszkowicz@email.cz>
2024-03-29 17:12:02 +0100

    initial commit

---
+++ src/main/java/com/games/fun/fun_games/FunGamesApplication.java
@@ -0,0 +1,13 @@
+package com.games.fun.fun_games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/games/fun/fun_games/FunGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.games.fun.fun_games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
