15bfa74fc2e2d508468b92415a0ad3ddc1afe053
tomas szyszkowicz <tomasszyszkowicz@email.cz>
2024-03-30 11:20:39 +0100

    added pexeso layout board

--- src/main/java/com/games/fun/fun_games/controller/HtmlController.java
+++ src/main/java/com/games/fun/fun_games/controller/HtmlController.java
@@ -2,17 +2,27 @@ package com.games.fun.fun_games.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.ui.Model;
 
 @Controller
 public class HtmlController {
-
-    @GetMapping("/index")
-    public String index() {
-        return "index";
-    }
 
     @GetMapping("/players-ui")
     public String players() {
         return "players_sql_ui";
     }
+
+    @GetMapping("/pexeso-menu")
+    public String pexesoMenu() {
+        return "pexeso-menu";
+    }
+
+    @GetMapping("/pexeso-game")
+    public String pexeso(@RequestParam(name = "difficulty", defaultValue = "easy") String difficulty, Model model) {
+        model.addAttribute("difficulty", difficulty);
+        return "pexeso";
+    }
+
 }
