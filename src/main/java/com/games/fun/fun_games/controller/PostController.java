0e120e4249e91f1d4608f704b68d390a2bbba64a
tomas szyszkowicz <tomasszyszkowicz@email.cz>
2024-04-07 21:51:54 +0200

    created backend endpoints for post entity

---
+++ src/main/java/com/games/fun/fun_games/controller/PostController.java
@@ -0,0 +1,43 @@
+package com.games.fun.fun_games.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.games.fun.fun_games.repository.PostRepository;
+import com.games.fun.fun_games.repository.UserRepository;
+import com.games.fun.fun_games.entity.Post;
+import com.games.fun.fun_games.entity.User;
+import com.games.fun.fun_games.dto.PostDto;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Post>> getPosts() {
+        List<Post> posts = postRepository.findAll();
+        return new ResponseEntity<>(posts, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Post> createPost(@RequestBody PostDto post) {
+        System.out.println(post.getUsername());
+        User user = userRepository.findByUsername(post.getUsername());
+        Post newPost = postRepository.save(new Post(user, post.getDateCreated(), post.getTitle(), post.getContent()));
+        return new ResponseEntity<>(newPost, HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/com/games/fun/fun_games/entity/Post.java
@@ -0,0 +1,76 @@
+package com.games.fun.fun_games.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String dateCreated;
+    private String title;
+    private String content;
+
+    public Post() {
+    }
+
+    public Post(User user, String dateCreated, String title, String content) {
+        this.user = user;
+        this.dateCreated = dateCreated;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(String dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
+
